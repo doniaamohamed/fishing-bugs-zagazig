10beec75fdfd3b83ceaf164fc44bae9b68b2700d
donia <doniaamohamed498@gmail.com>
2026-06-05 09:40:41 +0300

    add s to user

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-
-  get "user", to: "users#index"
+  # should be users not user
+  get "users", to: "users#index"
 end
